bb5f6cabddae1d87419b012ac822d2766ce6ccf2
michellexyeo <michboo@gmail.com>
2015-02-15 15:26:06 +0000

    blinking, lol

--- main.cpp
+++ main.cpp
@@ -17,9 +17,12 @@ int main() {
 	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
 
 	attron(COLOR_PAIR(2));
+	attron(A_BOLD | A_BLINK);
     printw("Tetris");
+	attroff(A_BOLD | A_BLINK);
 	attroff(COLOR_PAIR(2));
 	refresh();			
+	getch();
 	endwin();			
 
 	return 0;
